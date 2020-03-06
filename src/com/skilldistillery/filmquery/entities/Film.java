357f8b2519c930752352b16d5f4b4ad617fb2872
Diego Hoyos <hoyosdiegoa@gmail.com>
2020-03-06 16:44:06 -0700

    Initial commit

---
+++ src/com/skilldistillery/filmquery/entities/Film.java
@@ -0,0 +1,15 @@
+package com.skilldistillery.filmquery.entities;
+//create actor class as well
+//use double for decimal
+//If it can be null must use wrapper if it cannot be null must use primitive
+//for enum use string
+//get fields from desc film;
+//add getters, setters, tostring, equals
+//create constructor that can take any or all fields
+public class Film {
+	private int id;
+	private String rating;
+	private double replacementCost;
+	private Integer releaseYear;
+	private String specialFeatures;
+}
